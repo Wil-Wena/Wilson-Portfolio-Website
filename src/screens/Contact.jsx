2b670fa5ec5c130a94bd7481f5a1da2e0f7b9d38
Wilson Aballey <willywallet001@gmail.com>
2022-11-01 15:56:21 +0000

    Contact screen being worked upon 2

--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -10,12 +10,12 @@ const Contact = () => {
                 <p className='py-6'>You can get in touch with me through this form</p>
             </div>
             <div >
-                <form action="">
+                <form action="" className='flex flex-col w-full md:w-1/2'>
                     <input type="text" name='name' placeholder='Please enter your name'
-                    className='p-2 bg-transparent border-1 rounded-md text-white focus:outline-indigo-700' />
-                <input type="text" name='name' placeholder='Please enter your name'
-                    className='p-2 bg-transparent border-1 rounded-md text-white focus:outline-indigo-700' />
-               <textarea placeholder='Enter your message here' name="mesasge" rows="10" className='p-2 bg-transparent rounded:md border-1 focus:outline-indigo-700'></textarea>
+                    className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-indigo-700' />
+                <input type="text" name='name' placeholder='Please enter your email'
+                    className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-indigo-700' />
+               <textarea placeholder='Enter your message here' name="mesasge" rows="10" className='p-2 bg-transparent rounded:md border-dark-50 border-2 focus:outline-indigo-700'></textarea>
                 <button  className='bg-gray-800 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300 text-white'>
                     Submit
                 </button>
